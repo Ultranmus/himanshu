41f28c5603a8d526338a239697c73ff7a2ecfcc6
Ultranmus <mathpalhimanshu33@gmail.com>
2024-03-24 21:45:06 +0530

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+// Import /static for a static site
+import vercelStatic from '@astrojs/vercel/static';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: vercelStatic(),
   integrations: [react(), tailwind()]
 });
